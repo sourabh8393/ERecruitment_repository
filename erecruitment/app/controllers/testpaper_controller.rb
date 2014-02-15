2895021dfae7cff806dca2e332ec85210f0dee73
vaibhav <vaibhavpalav93@gmail.com>
2014-02-15 12:09:43 +0530

    vaibhav till pagination

--- erecruitment/app/controllers/testpaper_controller.rb
+++ erecruitment/app/controllers/testpaper_controller.rb
@@ -1,5 +1,5 @@
 class TestpaperController < ApplicationController
   def index
-  	@my_questions=My_question.order(:created_by)
+  	@my_questions=My_question.paginate(:page => params[:page], :per_page => 1).order(:created_by)
   end
 end
